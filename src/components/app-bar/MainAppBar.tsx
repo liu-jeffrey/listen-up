7abddc5df4478f555ab2cc4545e16a4546ac0f06
1298se <oliversong7h@gmail.com>
2020-01-25 15:40:11 -0500

    carried jeffrey

--- src/components/app-bar/MainAppBar.tsx
+++ src/components/app-bar/MainAppBar.tsx
@@ -2,7 +2,6 @@ import { IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuIcon from "@material-ui/icons/Menu";
 import { withStyles } from "@material-ui/styles";
 import clsx from "clsx";
